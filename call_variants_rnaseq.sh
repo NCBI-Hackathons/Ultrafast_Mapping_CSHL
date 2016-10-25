0f6db72ba908721822875cbc82d19a982de63702
Morgan Taschuk <morgan.taschuk@oicr.on.ca>
2016-10-25 04:45:17 +0000

    Fixed bugs in the call_variants scripts, added call_variants_rnaseq that follows GATK RNAseq best practices (for fun)

--- call_variants_rnaseq.sh
+++ call_variants_rnaseq.sh
@@ -13,14 +13,28 @@ THREADS=$10
 
 OUTNAME=`basename ${BAM} .bam`
 
+OUT1="${OUTDIR}/${OUTNAME}.split.bam"
+CMD="${JAVA} -Xms${GATK_MEM} -jar ${GATK_JAR} \
+	-T SplitNCigarReads \
+	-R ${REF} \
+	-I ${BAM} \
+	-o ${OUT1} \
+	-rf ReassignOneMappingQuality \
+	-RMQF 255 -RMQT 60 -U ALLOW_N_CIGAR_READS"
+
+
+OUT2="${OUTDIR}/${OUTNAME}.rnaseq.vcf"
 CMD="${JAVA} -jar -Xmx${GATK_MEM} ${GATK_JAR} \
      -R ${REF} \
      -T HaplotypeCaller \
-     -I ${BAM} \
+     -I ${OUT1} \
      --emitRefConfidence GVCF \
      --dbsnp ${DBSNP_VCF} \
-     -o ${OUTDIR}/${OUTNAME} \
+     -o ${OUT2} \
+     -dontUseSoftClippedBases \
+     -stand_call_conf 20.0 -stand_emit_conf 20.0 \
      -variant_index_type LINEAR -variant_index_parameter 128000  -U ALLOW_N_CIGAR_READS -nct ${THREADS}"
+
 #    -L ${INTERVALS} \
 
 
