eeaf0ab73e37da197cce9dd5e438a10831b9dcab
John Didion <github@didion.net>
2016-10-26 17:36:42 -0400

    refactor sra-based pipelines

--- evac/pipeline.py
+++ evac/pipeline.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Aligner-agnostic alignment pipeline that reads from SRA.
 """
+from inspect import inclass
 import logging
 import shlex
 from subprocess import Popen, PIPE
@@ -32,12 +33,44 @@ def stream_sra_reads(script_dir, args, progress, fifo1, fifo2):
         fifo2=fifo2))
     return Popen(cmd)
 
-def star_pipeline(args, script_dir):
-    with TempDir(dir=args.temp_dir) as workdir:
-        fifo1, fifo2 = workdir.mkfifos('Read1', 'Read2')
-        progress = args.progress and (
-            not args.quiet or args.log_level == 'ERROR' or args.log_file)
-        sra_proc = stream_sra_reads(script_dir, args, progress, fifo1, fifo2)
+# TODO: [JD] The use of pipes and shell=True is insecure and not the recommended
+# way of doing things, but I want to benchmark the alternative (chained Popens)
+# to make sure it's not any slower.
+
+def hisat_pipeline(args):
+    with open_(args.output, 'wb') as bam:
+        cmd = shlex.split("""
+            {exe} -p {threads} -x {index} --sra-acc {accn} {extra}
+                | sambamba view -S -t {threads} -f bam /dev/stdin
+                | sambamba sort -t {threads} /dev/stdin
+        """.format(
+            exe=args.hisat2 or "hisat2",
+            accn=args.sra_accession,
+            threads=args.threads,
+            index=args.index,
+            extra=args.aligner_args
+        ))
+        log.info("Running command: {}".format(' '.join(cmd)))
+        with Popen(cmd, stdout=bam, shell=True) as proc:
+            proc.wait()
+
+class SraPipeline(object):
+    def __init__(self, script_dir):
+        self.script_dir = script_dir
+    
+    def __call__(self, args):
+        with TempDir(dir=args.temp_dir) as workdir:
+            fifo1, fifo2 = workdir.mkfifos('Read1', 'Read2')
+            progress = args.progress and (
+                not args.quiet or args.log_level == 'ERROR' or args.log_file)
+            sra_proc = stream_sra_reads(
+                self.script_dir, args, progress, fifo1, fifo2)
+            with self.align(args) as align_proc:
+                for proc in (sra_proc, align_proc):
+                    proc.wait()
+
+class StarPipeline(SraPipeline):
+    def align(self, args):
         with open_(args.output, 'wb') as bam:
             cmd = shlex.split("""
                 {exe} --runThreadN {threads} --genomeDir {index}
@@ -56,93 +89,48 @@ def star_pipeline(args, script_dir):
                 extra=args.aligner_args
             ))
             log.info("Running command: {}".format(' '.join(cmd)))
-            align_proc = Popen(cmd, stdout=bam)
-            
-            for proc in (sra_proc, align_proc):
-                proc.wait()
+            yield Popen(cmd, stdout=bam)
 
-# TODO: [JD] The use of pipes and shell=True is insecure and not the recommended
-# way of doing things, but I want to benchmark the alternative (chained Popens)
-# to make sure it's not any slower.
-
-def hisat_pipeline(args, script_dir):
-    with open_(args.output, 'wb') as bam:
+class KallistoPipeline(SraPipeline):
+    def align(self, args):
+        libtype = ''
+        if 'F' in args.libtype:
+            libtype = '--fr-stranded'
+        elif 'R' in args.libtype:
+            libtype = '--rf-stranded'
         cmd = shlex.split("""
-            {exe} -p {threads} -x {index} --sra-acc {accn} {extra}
-                | sambamba view -S -t {threads} -f bam /dev/stdin
-                | sambamba sort -t {threads} /dev/stdin
+            {exe} quant -t {threads} -i {index} -o {output}
+                {libtype} {extra} {fifo1} {fifo2}
         """.format(
-            exe=args.hisat2 or "hisat2",
-            accn=args.sra_accession,
+            exe=args.kallisto or "kallisto",
             threads=args.threads,
             index=args.index,
-            extra=args.aligner_args
-        ))
+            output=args.output,
+            libtype=libtype,
+            extra=args.aligner_args,
+            fifo1=fifo1,
+            fifo2=fifo2))
         log.info("Running command: {}".format(' '.join(cmd)))
-        with Popen(cmd, stdout=bam, shell=True) as proc:
-            proc.wait()
+        yield Popen(cmd)
 
-# def kallisto_pipeline(args, script_dir):
-#     with TempDir(dir=args.temp_dir) as workdir:
-#         fifo1, fifo2 = workdir.mkfifos('Read1', 'Read2')
-#         libtype = ''
-#         if 'F' in args.libtype:
-#             libtype = '--fr-stranded'
-#         elif 'R' in args.libtype:
-#             libtype = '--rf-stranded'
-#         cmd = shlex.split("""
-#             {exe} quant -t {threads} -i {index} -o {output}
-#                 {libtype} {extra} {fifo1} {fifo2}
-#         """.format(
-#             exe=args.kallisto or "kallisto",
-#             threads=args.threads,
-#             index=args.index,
-#             output=args.output,
-#             libtype=libtype,
-#             extra=args.aligner_args,
-#             fifo1=fifo1,
-#             fifo2=fifo2))
-#         log.info("Running command: {}".format(' '.join(cmd)))
-#         with Popen(cmd) as proc:
-#             with FastqWriter(FifoWriter(fifo1, fifo2), args.batch_size) as writer:
-#                 for read_pair in sra_reader(
-#                         args.sra_accession,
-#                         batch_size=args.batch_size,
-#                         max_reads=args.max_reads):
-#                     writer(*read_pair)
-#             proc.wait()
-#
-# def salmon_pipeline(args, script_dir):
-#     with TempDir(dir=args.temp_dir) as workdir:
-#         fifo1, fifo2 = workdir.mkfifos('Read1', 'Read2')
-#         cmd = shlex.split("""
-#             {exe} quant -p {threads} -i {index} -l {libtype}
-#                 {extra} -1 {fifo1} -2 {fifo2} -o {output}
-#         """.format(
-#             exe=args.salmon or 'salmon',
-#             threads=args.threads,
-#             index=args.index,
-#             libtype=args.libtype,
-#             output=args.output,
-#             extra=args.aligner_args,
-#             fifo1=fifo1,
-#             fifo2=fifo2))
-#         log.info("Running command: {}".format(' '.join(cmd)))
-#         writer_proc =
-#         reader_proc = Popen(cmd)
-#         for proc in (writer_proc, reader_proc):
-#             proc.wait()
-#
-#
-#             with FastqWriter(FifoWriter(fifo1, fifo2), args.batch_size) as writer:
-#                 for read_pair in sra_reader(
-#                         args.sra_accession,
-#                         batch_size=args.batch_size,
-#                         max_reads=args.max_reads):
-#                     writer(*read_pair)
-            
+class SalmonPipeline(SraPipeline):
+    def align(self, args):
+        cmd = shlex.split("""
+            {exe} quant -p {threads} -i {index} -l {libtype}
+                {extra} -1 {fifo1} -2 {fifo2} -o {output}
+        """.format(
+            exe=args.salmon or 'salmon',
+            threads=args.threads,
+            index=args.index,
+            libtype=args.libtype,
+            output=args.output,
+            extra=args.aligner_args,
+            fifo1=fifo1,
+            fifo2=fifo2))
+        log.info("Running command: {}".format(' '.join(cmd)))
+        yield Popen(cmd)
 
-def sra_to_fastq_pipeline(args, script_dir):
+def sra_to_fastq_pipeline(args):
     fq1_path = args.output + '.1.fq'
     fq2_path = args.output + '.2.fq'
     with open(fq1_path, 'wt') as fq1, open(fq2_path, 'wt') as fq2:
@@ -153,7 +141,7 @@ def sra_to_fastq_pipeline(args, script_dir):
             fq1.write("@{}\n{}\n+\n{}\n".format(*read1))
             fq2.write("@{}\n{}\n+\n{}\n".format(*read2))
 
-def head_pipeline(args, script_dir):
+def head_pipeline(args):
     for read1, read2 in sra_reader(
             args.sra_accession,
             batch_size=args.batch_size,
@@ -164,10 +152,10 @@ def head_pipeline(args, script_dir):
         print('  ' + '\t'.join(read2[1:]))
 
 pipelines = dict(
-    star=star_pipeline,
     hisat=hisat_pipeline,
-    #kallisto=kallisto_pipeline,
-    #salmon=salmon_pipeline,
+    star=StarPipeline,
+    kallisto=KallistoPipeline,
+    salmon=SalmonPipeline,
     fastq=sra_to_fastq_pipeline,
     head=head_pipeline)
 
@@ -189,7 +177,9 @@ def run_pipeline(args, script_dir):
     """
     setup_logging(args)
     pipeline = pipelines[args.pipeline]
-    pipeline(args, script_dir)
+    if isclass(pipeline):
+        pipeline = pipeline(script_dir)
+    pipeline(args)
 
 def setup_logging(args):
     if not logging.root.handlers:
