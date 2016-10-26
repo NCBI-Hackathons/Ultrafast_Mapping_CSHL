99b0de182376ca2b02d31a1cc88fb38f72a87ce7
upendrak <upendrakumar.devisetty@googlemail.com>
2016-10-26 17:00:44 +0000

    modify scripts

--- run_mapping_sort.sh
+++ run_mapping_sort.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-set -e -o pipefail
-
 SAM=$1
 OUT=$2
 
-CMD="hisat2 -p 8 -x /home/data/hg38/genome --sra-acc SRR1616919 --rg-id test1 --rg PU:99.9.Z --rg SM:SAMN03120904 --rg PL:ILLUMINA --rg LB:M407 -S ${SAM}"
+CMD="hisat2 -p 8 -x /home/data/hg38/genome --sra-acc SRR2056440 --rg-id test1 --rg PU:99.9.Z --rg SM:SAMN03120904 --rg PL:ILLUMINA --rg LB:M407 -S ${SAM}"
 CMD="sambamba_v0.6.5 view -S -t 8 -f bam ${SAM} | sambamba_v0.6.5 sort -t 8 -o ${OUT} /dev/stdin"
 
 echo $CMD
